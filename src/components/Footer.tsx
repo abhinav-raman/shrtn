554e2ae9f4203e8d333b194637ee4fbe2476163b
Abhinav Raman <abhinavraman99@gmail.com>
2022-08-24 22:19:33 +0530

    Reinstalled packages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <div>
+    <footer>
       Footer
-    </div>
+    </footer>
   );
 }
 
--- src/components/HomeLogin.tsx
+++ src/components/HomeLogin.tsx
@@ -7,9 +7,9 @@ type HomeLoginProps = {
 }
 
 const HomeLogin = ({ userData }: HomeLoginProps) => {
-	const { data: session } = useSession();
+	// const { data: session } = useSession();
 
-	if (!session) {
+	if (!userData) {
 		return (
 			<section className="w-1/2 flex justify-center flex-col p-4">
 				<h1 className="w-full text-center text-2xl mb-4">
@@ -28,8 +28,8 @@ const HomeLogin = ({ userData }: HomeLoginProps) => {
 
 	return (
 		<section className="w-1/2 flex justify-center flex-col p-4">
-			<h1 className="w-full text-right text-4xl my-2">{session.user?.name}</h1>
-			<h3 className="w-full text-right text-xl my-2">{session.user?.email}</h3>
+			<h1 className="w-full text-right text-4xl my-2">{userData.user?.name}</h1>
+			<h3 className="w-full text-right text-xl my-2">{userData.user?.email}</h3>
 			<div className="w-full text-right mt-4 mb-4">
 				<button className="bg-gradient-to-r from-violet-800 to-blue-800 text-white rounded py-1 px-3 mr-4">
 					Manage Account
