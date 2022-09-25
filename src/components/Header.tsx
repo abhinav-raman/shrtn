bcf83db8a61fcf57e51435800f0e2134b81f659e
Abhinav Raman <abhinavraman99@gmail.com>
2022-09-25 14:13:04 +0530

    mobile related aesthetic changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
 			<h1 className="text-5xl font-extrabold bg-gradient-to-r from-violet-800 to-blue-800 dark:from-violet-400 dark:to-blue-400 text-transparent bg-clip-text">
 				shrtn
 			</h1>
-			<h1 className="text-5xl font-thin hidden md:inline">&nbsp;your links</h1>
+			<h1 className="text-5xl font-thin hidden md:inline italic">&nbsp;your links</h1>
 		</header>
 	);
 };
